e25886d911fa624b1794361e92b9cbd904a551b6
thecodehen <6582598+thecodehen@users.noreply.github.com>
2025-06-08 13:15:37 +0200

    Simplify code with utils

--- floraAdamFactoredMAS.py
+++ floraAdamFactoredMAS.py
@@ -1,35 +1,11 @@
-import numpy as np
-from torch.optim.optimizer import Optimizer
-from typing import Any, Dict, Iterable, Optional, Sequence, Union
-import torch
 import math
+from typing import Any, Dict, Iterable, Union
 
-def stable_randn(
-    shape: Union[int, Sequence[int]],
-    seed: int,
-    device: Optional[Union[str, torch.device]] = None,
-    dtype: Optional[torch.dtype] = torch.float32,
-) -> torch.Tensor:
-    if device is None:
-        device = torch.device("cpu")
-    generator = torch.Generator(device=device).manual_seed(seed)
-    rn = torch.randn(shape, generator=generator, device=generator.device, dtype=dtype)
-    return rn
+import numpy as np
+import torch
+from torch.optim.optimizer import Optimizer
 
-def next_seed(seed: int, adv: int = 0xF) -> int:
-    """
-    This is a naive helper function to generate a new seed from the given seed.
-    """
-    generator = torch.Generator().manual_seed(seed)
-    return torch.randint(
-        0, torch.iinfo(torch.int64).max, (adv,), generator=generator, device=generator.device
-    ).tolist()[-1]
-
-def split_seed(seed: int) -> tuple[int, int]:
-    generator = torch.Generator().manual_seed(seed)
-    return tuple(
-        torch.randint(0, torch.iinfo(torch.int64).max, (2,), generator=generator, device=generator.device).tolist()
-    )
+from utils import stable_randn, next_seed, split_seed
 
 class FloraAdamFactoredMAS(Optimizer):
     def __init__(
