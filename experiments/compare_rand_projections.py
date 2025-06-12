2f7a2906db5f931f3ad0aa34076c17ca1392029e
thecodehen <6582598+thecodehen@users.noreply.github.com>
2025-06-12 15:36:50 +0200

    Add clip_norm

--- experiments/compare_rand_projections.py
+++ experiments/compare_rand_projections.py
@@ -6,79 +6,14 @@ import torch
 import torch.nn as nn
 
 import numpy as np
-from sklearn.metrics import accuracy_score
-from tqdm import tqdm
 
 from flora import FloraAdam
 from models import SimpleNeuralNetwork
+from training import (
+    train_model,
+    evaluate_model,
+)
 from utils import get_mnist
-
-def train_one_epoch(model, optimizer, loss_fn, batch_inputs, batch_labels, epoch_index, device=None):
-    # Zero your gradients for every batch!
-    optimizer.zero_grad()
-
-    # Make predictions for this batch
-    outputs = model(batch_inputs)
-
-    # Compute the loss and its gradients
-    loss = loss_fn(outputs, batch_labels)
-    loss.backward()
-
-    # Adjust learning weights
-    optimizer.step()
-
-    return loss.item()
-
-def train_model(model, epochs, train_dataset, validation_dataset, loss_fn, optimizer, device=None):
-    train_inputs = train_dataset['inputs'].to(device)
-    train_labels = train_dataset['labels'].to(device)
-    validation_inputs = validation_dataset['inputs'].to(device)
-    validation_labels = validation_dataset['labels'].to(device)
-
-
-    train_losses = []
-    validation_losses = []
-    for epoch in tqdm(range(epochs)):
-        # Make sure gradient tracking is on, and do a pass over the data
-        model.train(True)
-        train_loss = train_one_epoch(model, optimizer, loss_fn, train_inputs, train_labels, epoch, device=device)
-        train_losses.append(train_loss)
-
-        # Set the model to evaluation mode, disabling dropout and using population
-        # statistics for batch normalization.
-        model.eval()
-
-        # Disable gradient computation and reduce memory consumption.
-        with torch.no_grad():
-            # Make predictions for this batch
-            voutputs = model(validation_inputs)
-            validation_loss = loss_fn(voutputs, validation_labels)
-            validation_losses.append(validation_loss.item())
-
-        # Track best performance, and save the model's state
-        # if validation_loss < best_vloss:
-        #     best_vloss = validation_loss
-        #     model_path = 'model_{}_{}'.format(timestamp, epoch)
-        #     torch.save(model.state_dict(), model_path)
-
-        epoch += 1
-
-    return train_losses, validation_losses
-
-def evaluate_model(model, test_dataset, device=None):
-    inputs = test_dataset['inputs'].to(device)
-
-    with torch.no_grad():
-        outputs = model(inputs).argmax(dim=1)
-
-    all_labels = test_dataset['labels']
-    all_preds = outputs.cpu()
-
-    assert len(all_labels) == len(all_preds)
-
-    acc = accuracy_score(all_labels, all_preds)
-
-    return all_preds, all_labels, acc
 
 def main():
     parser = argparse.ArgumentParser()
@@ -89,6 +24,7 @@ def main():
     parser.add_argument('--hidden_size', type=int, default=1024)
     parser.add_argument('--num_layers', type=int, default=1)
     parser.add_argument('--rand_distribution', type=str, default='normal')
+    parser.add_argument('--clip_norm', action='store_true')
     parser.add_argument('--output_path', type=str, default='results')
 
     args = parser.parse_args()
@@ -116,14 +52,14 @@ def main():
 
     train_dataset, validation_dataset, test_dataset = get_mnist()
 
-    train_losses, validation_losses = train_model(model, args.epochs, train_dataset, validation_dataset, loss_fn, optimizer, device=device)
+    train_losses, validation_losses = train_model(model, args.epochs, train_dataset, validation_dataset, loss_fn, optimizer, args.clip_norm, device=device)
     _, _, acc = evaluate_model(model, test_dataset, device=device)
 
     print('Accuracy: ', acc * 100, '%')
 
     # save the models
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-    save_path = pathlib.Path('results') / f"{args.rand_distribution}-rank_{args.rank}-seed_{args.seed}-{timestamp}"
+    save_path = pathlib.Path('results') / 'flora' / f"rand_distribution_{args.rand_distribution}-rank_{args.rank}-seed_{args.seed}-{timestamp}"
     save_path.mkdir(parents=True, exist_ok=True)
     torch.save(model.state_dict(), save_path / f"model.pth")
     np.save(save_path / f"train_losses.npy", train_losses)
