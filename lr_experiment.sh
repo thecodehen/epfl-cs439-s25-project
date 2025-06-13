9b7cc836c000f60a010211bde0839ab45fa4ab16
pirxtion <pirxtion@gmail.com>
2025-06-13 02:55:19 +0200

    lr experiment

--- lr_experiment.sh
+++ lr_experiment.sh
@@ -1,14 +1,14 @@
 for seed in 42 123 888; do
-    python -m experiments.run_flora --rand_distribution normal --rank 8 --hidden_size 256 --seed $seed --lr 0.05
-    python -m experiments.run_flora --rand_distribution normal --rank 8 --hidden_size 256 --seed $seed --lr 0.01
-    python -m experiments.run_flora --rand_distribution normal --rank 8 --hidden_size 256 --seed $seed --lr 0.005
-    python -m experiments.run_flora --rand_distribution normal --rank 8 --hidden_size 256 --seed $seed --lr 0.001
-    python -m experiments.run_flora --rand_distribution normal --rank 8 --hidden_size 256 --seed $seed --lr 0.0005
-    python -m experiments.run_flora --rand_distribution normal --rank 8 --hidden_size 256 --seed $seed --lr 0.0001
-    python -m experiments.run_adam --hidden_size 256 --seed $seed --lr 0.05
-    python -m experiments.run_adam --hidden_size 256 --seed $seed --lr 0.01
-    python -m experiments.run_adam --hidden_size 256 --seed $seed --lr 0.005
-    python -m experiments.run_adam --hidden_size 256 --seed $seed --lr 0.001
-    python -m experiments.run_adam --hidden_size 256 --seed $seed --lr 0.0005
-    python -m experiments.run_adam --hidden_size 256 --seed $seed --lr 0.0001
+    python -m experiments.run_flora --rand_distribution normal --rank 16 --hidden_size 256 --seed $seed --lr 0.05 --epochs 3000
+    python -m experiments.run_flora --rand_distribution normal --rank 16 --hidden_size 256 --seed $seed --lr 0.01 --epochs 3000
+    python -m experiments.run_flora --rand_distribution normal --rank 16 --hidden_size 256 --seed $seed --lr 0.005 --epochs 3000
+    python -m experiments.run_flora --rand_distribution normal --rank 16 --hidden_size 256 --seed $seed --lr 0.001 --epochs 3000
+    python -m experiments.run_flora --rand_distribution normal --rank 16 --hidden_size 256 --seed $seed --lr 0.0005 --epochs 3000
+    python -m experiments.run_flora --rand_distribution normal --rank 16 --hidden_size 256 --seed $seed --lr 0.0001 --epochs 3000
+    python -m experiments.run_adam --hidden_size 256 --seed $seed --lr 0.05 --epochs 3000
+    python -m experiments.run_adam --hidden_size 256 --seed $seed --lr 0.01 --epochs 3000
+    python -m experiments.run_adam --hidden_size 256 --seed $seed --lr 0.005 --epochs 3000
+    python -m experiments.run_adam --hidden_size 256 --seed $seed --lr 0.001 --epochs 3000
+    python -m experiments.run_adam --hidden_size 256 --seed $seed --lr 0.0005 --epochs 3000
+    python -m experiments.run_adam --hidden_size 256 --seed $seed --lr 0.0001 --epochs 3000
 done
