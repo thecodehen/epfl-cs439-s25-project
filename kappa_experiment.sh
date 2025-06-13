2b45fba7e6d5e717dca7e13d0ab832769b6e45f0
Jingxuan Sun <89790849+PirxTion@users.noreply.github.com>
2025-06-13 03:55:08 +0200

    clean code base

--- kappa_experiment.sh
+++
@@ -1,5 +0,0 @@
-for seed in 42 123 88; do
-    for kappa in 10 50 100 200 500 1000; do
-        python -m experiments.run_flora --rand_distribution normal --rank 16 --kappa $kappa --hidden_size 256 --seed $seed --lr 0.0005 --epochs 3000
-    done
-done
--- lr_experiment.sh
+++
@@ -1,6 +0,0 @@
-for seed in 42 123 888; do
-    for lr in 0.05 0.01 0.005 0.001 0.0005 0.0001 do
-        python -m experiments.run_flora --rand_distribution normal --rank 16 --hidden_size 256 --seed $seed --lr $lr --epochs 3000
-        python -m experiments.run_adam --hidden_size 256 --seed $seed --lr $lr --epochs 3000
-    done
-done
