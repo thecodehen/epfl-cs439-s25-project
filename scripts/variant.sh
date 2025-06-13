debd6082663590f6b601e89f54a7b9a50b52f3da
pirxtion <pirxtion@gmail.com>
2025-06-13 04:40:33 +0200

    fig for convergence

--- scripts/variant.sh
+++ scripts/variant.sh
@@ -1,4 +1,4 @@
 for seed in 42 123 888; do
-    python -m experiments.run_floraaf.py --seed $seed --epochs 3000 --lr 0.0005 -- rank 16 --hidden_size 256
-    python -m experiments.run_floradr.py --seed $seed --epochs 3000 --lr 0.0005 -- rankFrom 16 --rankTo 2 --hidden_size 256 
+    python -m experiments.run_floraaf --seed $seed --epochs 3000 --lr 0.0005 --rank 16 --hidden_size 256
+    python -m experiments.run_floradr --seed $seed --epochs 3000 --lr 0.0005 --rankFrom 16 --rankTo 2 --hidden_size 256 
 done
